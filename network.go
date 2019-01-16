05224b29dc39df2dc1c3dd26972a2996dabde66c
zoetrope <a.ikezoe@gmail.com>
2019-01-16 18:16:43 +0900

    Revert "Merge pull request #78 from cybozu-go/suppress-error-message"
    This reverts commit 673990466b63e02d71c9412101427bd97a778711, reversing
    changes made to ec77ef53176d8319f2ca29799f16e9136efd9ca4.

--- network.go
+++ network.go
@@ -236,8 +236,7 @@ func CleanupNetworks(r *Runtime, c *Cluster) {
 	execCommandsForce(cmds)
 }
 
-// Destroy deletes all created tap devices, then the bridge.
-// Do not explicitly delete veth devices, because it will be removed along with the network namespace.
+// Destroy deletes all created tap and veth devices, then the bridge.
 func (n *Network) Destroy() error {
 	if n.v4forwarded {
 		setForwarding(v4ForwardKey, false)
@@ -249,6 +248,9 @@ func (n *Network) Destroy() error {
 	cmds := [][]string{}
 	for _, name := range n.tapNames {
 		cmds = append(cmds, []string{"ip", "tuntap", "delete", name, "mode", "tap"})
+	}
+	for _, name := range n.vethNames {
+		cmds = append(cmds, []string{"ip", "link", "delete", name})
 	}
 	cmds = append(cmds, []string{"ip", "link", "delete", n.Name, "type", "bridge"})
 
