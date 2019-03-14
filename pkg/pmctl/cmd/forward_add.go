7e980982103262712650e54fbd4abbeb90fff2a4
mitz <mitz@cybozu.co.jp>
2019-03-14 18:27:32 +0900

    Use nsenter to run socat instead of socat

--- pkg/pmctl/cmd/forward_add.go
+++ pkg/pmctl/cmd/forward_add.go
@@ -5,11 +5,13 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"os"
 	"os/exec"
 	"strconv"
 	"strings"
 
 	"github.com/cybozu-go/log"
+	"github.com/cybozu-go/placemat/web"
 	"github.com/cybozu-go/well"
 	"github.com/spf13/cobra"
 )
@@ -52,14 +54,23 @@ var forwardAddCmd = &cobra.Command{
 				return err
 			}
 
-			return exec.CommandContext(ctx,
+			var status web.PodStatus
+			err = getJSON(ctx, "/pods/"+forward.PodName, nil, &status)
+			if err != nil {
+				return err
+			}
+
+			c := exec.CommandContext(ctx,
 				"systemd-run",
 				fmt.Sprintf("--unit=pmctl-forward-%d.service", forward.LocalPort),
 				fmt.Sprintf("--description=%s", forwardJSON),
 				"socat",
 				fmt.Sprintf("tcp-listen:%d,fork,reuseaddr", forward.LocalPort),
-				fmt.Sprintf("exec:ip netns exec %s socat STDIO tcp-connect\\:%s\\:%d,nofork",
-					forward.PodName, forward.RemoteHost, forward.RemotePort)).Run()
+				fmt.Sprintf("exec:nsenter -n -t %d socat STDIO tcp-connect\\:%s\\:%d,nofork",
+					status.PID, forward.RemoteHost, forward.RemotePort))
+			c.Stdout = os.Stdout
+			c.Stderr = os.Stderr
+			return c.Run()
 		})
 		well.Stop()
 		err := well.Wait()
@@ -67,4 +78,8 @@ var forwardAddCmd = &cobra.Command{
 			log.ErrorExit(err)
 		}
 	},
+}
+
+func init() {
+	forwardCmd.AddCommand(forwardAddCmd)
 }
