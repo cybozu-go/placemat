3b8984e78c235a90d2a61d39ad625ace379d0446
morimoto-cybozu <kenji_morimoto@cybozu.co.jp>
2019-12-06 11:07:15 +0000

    Fix
    Signed-off-by: morimoto-cybozu <kenji_morimoto@cybozu.co.jp>

--- mtest/suites.go
+++ mtest/suites.go
@@ -4,8 +4,8 @@ import . "github.com/onsi/ginkgo"
 
 // FunctionsSuite is a test suite that tests small test cases
 var FunctionsSuite = func() {
-	//Context("cleanup", TestCleanup)
-	//Context("example", TestExample)
-	//Context("pod", TestPod)
+	Context("cleanup", TestCleanup)
+	Context("example", TestExample)
+	Context("pod", TestPod)
 	Context("BMC", TestBMC)
 }
