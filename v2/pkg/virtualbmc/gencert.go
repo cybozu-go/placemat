33a40b434f710185c97cff12f88a08b785f7c030
Yusuke Suzuki <yusuke-suzuki@cybozu.co.jp>
2021-03-17 09:01:22 +0900

    Fix BMC server startup failure
    Signed-off-by: Yusuke Suzuki <yusuke-suzuki@cybozu.co.jp>

--- v2/pkg/virtualbmc/gencert.go
+++ v2/pkg/virtualbmc/gencert.go
@@ -8,16 +8,14 @@ import (
 	"encoding/pem"
 	"fmt"
 	"math/big"
-	"os"
-	"path/filepath"
 	"strings"
 	"time"
 )
 
-func generateCertificate(host, outDir string, validFor time.Duration) (string, string, error) {
+func generateCertificate(host string, validFor time.Duration) ([]byte, []byte, error) {
 	priv, err := rsa.GenerateKey(rand.Reader, 4096)
 	if err != nil {
-		return "", "", fmt.Errorf("failed to generate key: %w", err)
+		return nil, nil, fmt.Errorf("failed to generate key: %w", err)
 	}
 
 	keyUsage := x509.KeyUsageDigitalSignature | x509.KeyUsageKeyEncipherment
@@ -39,36 +37,16 @@ func generateCertificate(host, outDir string, validFor time.Duration) (string, s
 
 	certBytes, err := x509.CreateCertificate(rand.Reader, &template, &template, priv.Public(), priv)
 	if err != nil {
-		return "", "", fmt.Errorf("failed to create certificate: %w", err)
-	}
-
-	_, err = os.Stat(outDir)
-	switch {
-	case err == nil:
-	case os.IsNotExist(err):
-		err = os.MkdirAll(outDir, 0755)
-		if err != nil {
-			return "", "", fmt.Errorf("failed to create output directory: %w", err)
-		}
-	default:
-		return "", "", fmt.Errorf("stat %s failed: %w", outDir, err)
+		return nil, nil, fmt.Errorf("failed to create certificate: %w", err)
 	}
 
 	privBytes, err := x509.MarshalPKCS8PrivateKey(priv)
 	if err != nil {
-		return "", "", fmt.Errorf("failed to marshal private key: %w", err)
+		return nil, nil, fmt.Errorf("failed to marshal private key: %w", err)
 	}
 
-	cert := filepath.Join(outDir, "cert.pem")
-	if err := outputPEM(cert, "CERTIFICATE", certBytes); err != nil {
-		return "", "", err
-	}
-	key := filepath.Join(outDir, "key.pem")
-	if err := outputPEM(key, "PRIVATE KEY", privBytes); err != nil {
-		return "", "", err
-	}
-
-	return cert, key, nil
+	return pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: certBytes}),
+		pem.EncodeToMemory(&pem.Block{Type: "PRIVATE KEY", Bytes: privBytes}), nil
 }
 
 func dnsAliases(host string) []string {
@@ -78,24 +56,4 @@ func dnsAliases(host string) []string {
 		aliases[i] = strings.Join(parts[0:len(parts)-i], ".")
 	}
 	return aliases
-}
-
-func outputPEM(fname string, pemType string, data []byte) error {
-	f, err := os.OpenFile(fname, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
-	if err != nil {
-		return fmt.Errorf("failed to open %s: %w", fname, err)
-	}
-	defer f.Close()
-
-	err = pem.Encode(f, &pem.Block{Type: pemType, Bytes: data})
-	if err != nil {
-		return fmt.Errorf("failed to encode: %w", err)
-	}
-
-	err = f.Sync()
-	if err != nil {
-		return fmt.Errorf("failed to fsync: %w", err)
-	}
-
-	return nil
 }
